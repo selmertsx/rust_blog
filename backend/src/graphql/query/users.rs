6a69f17cdb20aab94c719a545bc9c3e980b09ea4
shuhei mroioka <selmertsx@gmail.com>
2022-02-08 01:34:23 +0900

    add user record

--- backend/src/graphql/query/users.rs
+++ backend/src/graphql/query/users.rs
@@ -2,9 +2,16 @@
 use crate::graphql::object::{User, UserKind};
 
 pub fn query() -> Vec<User> {
-    vec![User {
-        id: 1,
-        kind: UserKind::Admin,
-        name: "user1".into(),
-    }]
+    vec![
+        User {
+            id: 1,
+            kind: UserKind::Admin,
+            name: "user1".into(),
+        },
+        User {
+            id: 2,
+            kind: UserKind::Admin,
+            name: "user2".into(),
+        }
+    ]
 }
