99099a94a9b783d2289edcfdd20d9fe052a47aa7
shuhei mroioka <selmertsx@gmail.com>
2022-02-11 00:33:17 +0900

    postsを作る

--- backend/migrations/2022-02-09-155047_create_users/up.sql
+++ backend/migrations/2022-02-09-155047_create_users/up.sql
@@ -1,4 +1,14 @@
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
-  name VARCHAR NOT NULL
-)
+  name VARCHAR NOT NULL,
+  email VARCHAR NOT NULL UNIQUE
+);
+
+CREATE TABLE posts (
+  id SERIAL PRIMARY KEY,
+  user_id integer NOT NULL,
+  title VARCHAR NOT NULL,
+  body TEXT
+);
+
+CREATE INDEX posts_user_id ON posts (user_id);
--- backend/src/schema.rs
+++ backend/src/schema.rs
@@ -1,6 +1,21 @@
 table! {
+    posts (id) {
+        id -> Int4,
+        user_id -> Int4,
+        title -> Varchar,
+        body -> Nullable<Text>,
+    }
+}
+
+table! {
     users (id) {
         id -> Int4,
         name -> Varchar,
+        email -> Varchar,
     }
 }
+
+allow_tables_to_appear_in_same_query!(
+    posts,
+    users,
+);
