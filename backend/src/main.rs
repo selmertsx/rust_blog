6da3bdbb672bcd1ac50391e23c0aa057acb8f04b
shuhei mroioka <selmertsx@gmail.com>
2022-02-11 00:08:27 +0900

    不必要なレスポンスを削除

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,6 +1,6 @@
 //! This example demonstrates async/await usage with warp.
 
-use warp::{http::Response, Filter};
+use warp::Filter;
 mod context;
 pub use context::Context;
 
@@ -10,15 +10,6 @@ use graphql::{ schema };
 #[tokio::main]
 async fn main() {
     let log = warp::log("warp_server");
-
-    let homepage = warp::path::end().map(|| {
-        Response::builder()
-            .header("content-type", "text/html")
-            .body(
-                "<html><h1>juniper_warp</h1><div>visit <a href=\"/graphiql\">/graphiql</a></html>",
-            )
-    });
-
     let state = warp::any().map(|| Context);
     let graphql_filter = juniper_warp::make_graphql_filter(schema(), state.boxed());
 
@@ -26,7 +17,6 @@ async fn main() {
         warp::get()
             .and(warp::path("graphiql"))
             .and(juniper_warp::graphiql_filter("/graphql", None))
-            .or(homepage)
             .or(warp::path("graphql").and(graphql_filter))
             .with(log),
     )
